dc77f1868b764e900a19551b10e584fbb378a53d
hbauhvagner <hbauhvagner@gmail.com>
2023-09-12 00:42:04 -0300

    Lista dinâmica

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,10 @@
+import styles from './Inicio.module.css';
 import Banner from "../../components/Banner";
 import Cabecalho from "../../components/Cabecalho";
 import Card from "../../components/Card";
 import Rodape from "../../components/Rodape";
 import Titulo from "../../components/Titulo";
+import videos from '../../json/db.json';
 
 function Inicio() {
     return (
@@ -12,7 +14,11 @@ function Inicio() {
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
-            <Card id='1' titulo='Gato bonifacio' capa='https://i.pinimg.com/564x/42/94/71/429471db883f04340eb28c7de81dbb5d.jpg' />
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Rodape />
         </>
     )
