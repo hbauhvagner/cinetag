a1f24a904d83119e7b2d0e4ee50cee0b3a4cf831
hbauhvagner <hbauhvagner@gmail.com>
2023-09-13 23:19:00 -0300

    Desafio: construindo páginas

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,19 @@
+import Banner from '../../components/Banner';
+import Card from '../../components/Card';
+import Titulo from '../../components/Titulo';
 import styles from './Favoritos.module.css';
 
 function Favoritos() {
     return (
-        <></>
+        <>
+            <Banner imagem='favoritos' />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id='1' titulo='Gato bonifácio' capa='https://i.pinimg.com/564x/42/94/71/429471db883f04340eb28c7de81dbb5d.jpg' />
+            </section>
+        </>
     )   
 }
 
