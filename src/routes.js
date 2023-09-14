96f95a0397caa318e55400b6bf0926a7c8213af6
hbauhvagner <hbauhvagner@gmail.com>
2023-09-13 23:37:32 -0300

    Contextos

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,19 @@ import Container from "./components/Container";
 import Favoritos from "./pages/Favoritos";
 import Inicio from "./pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FavoritosProvider from "./context/Favoritos";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Cabecalho />
             <Container>
-                <Routes>
-                    <Route path="/" element={<Inicio />} />
-                    <Route path="/favoritos" element={<Favoritos />} />
-                </Routes>
+                <FavoritosProvider>
+                    <Routes>
+                        <Route path="/" element={<Inicio />} />
+                        <Route path="/favoritos" element={<Favoritos />} />
+                    </Routes>
+                </FavoritosProvider>
             </Container>
             <Rodape />
         </BrowserRouter>
