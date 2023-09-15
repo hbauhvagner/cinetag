1e775674a0f3734745959b484504fcf6d441d659
hbauhvagner <hbauhvagner@gmail.com>
2023-09-15 00:30:13 -0300

    Fetch com parâmetros

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -2,14 +2,20 @@ import { useParams } from 'react-router-dom';
 import Banner from '../../components/Banner';
 import Titulo from '../../components/Titulo';
 import styles from './Player.module.css';
-import videos from '../../json/db.json';
 import NaoEncontrada from '../NaoEncontrada';
+import { useEffect, useState } from 'react';
 
 function Player() {
+    const [video, setVideo] = useState([]);
     const parametros = useParams();
-    const video = videos.find((video) => {
-        return video.id === Number(parametros.id);
-    })
+
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/hbauhvagner/cinetag-api/videos?id=${parametros.id}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideo(...dados)
+            })
+    }, [])
 
     if (!video) {
         return <NaoEncontrada />
